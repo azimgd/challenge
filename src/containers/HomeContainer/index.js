ffc9c31671996524a80763ea5516117aec2b2813
Azim Gadzhiagayev <me@azimgd.com>
2017-01-24 15:41:19 +0500

    magento xml-rpc integration attempt

--- src/containers/HomeContainer/index.js
+++ src/containers/HomeContainer/index.js
@@ -1,6 +1,28 @@
 import React from 'react';
 import styles from 'containers/HomeContainer/index.scss';
 
+var MagentoAPI = require('magento-xmlrpc');
+var magento = new MagentoAPI({
+  host: 'www-test.unumotors.com',
+  port: 443,
+  path: '/en/api/xmlrpc/',
+  login: 'unu-challenge',
+  pass: 'unu-challenge'
+});
+
+
+setTimeout(() => {
+
+  magento.login(function(err, sessId) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+  });
+
+
+}, 3000);
+
 const HomeContainer = () => (
   <div className={styles.block}>
     <div className={styles.component}>
