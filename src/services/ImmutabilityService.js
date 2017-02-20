d63527c41ce4d066dad0e308bcca5778ca5944ac
Azim Gadzhiagayev <me@azimgd.com>
2017-02-20 15:24:44 +0500

    Basic test cases

--- src/services/ImmutabilityService.js
+++ src/services/ImmutabilityService.js
@@ -1,40 +1,83 @@
 import { newContext } from 'immutability-helper';
 const immutability = newContext();
 
-const arrayIntersection = (first, second) => {
-  if (!Array.isArray(first) || !Array.isArray(second)) {
-    return [];
-  }
+const isArray = input => Array.isArray(input);
+const isObject = input => input !== null && typeof input === 'object';
+const isInteger = input => parseInt(input) === input;
+
+/**
+ * Creates an array of unique, intersecting values from provided arrays
+ */
+export const arrayIntersection = (first, second) => {
+  if (!isArray(first) || !isArray(second)) { return []; }
   return first.filter((item) => second.includes(item));
 };
-const findIndex = (array, query) => array.findIndex((item) => item === query);
 
-const paginationSlice = (pagination, original) =>
-  original.slice((pagination.page - 1) * pagination.perPage, pagination.page * pagination.perPage);
+/**
+ * Returns index of provided element in array
+ */
+export const findIndex = (array, query) => {
+  if (!isArray(array)) { return false; }
+  return array.findIndex((item) => item === query)
+};
 
-const searchByQuery = (searchQuery, original) => original.filter(item => {
-  const target = item.joke.toLowerCase();
-  const query = searchQuery.toLowerCase();
-  return target.includes(query);
-});
+/**
+ * Slices contents from data according to provided pagination
+ */
+export const paginationSlice = (pagination, original) => {
+  if (!isObject(pagination) || !isArray(original)) { return []; }
+  return original.slice((pagination.page - 1) * pagination.perPage, pagination.page * pagination.perPage);
+};
 
-const filterByCategories = (categoriesQuery, original) => original.filter(item =>
-  arrayIntersection(item.categories, categoriesQuery).length
-);
+/**
+ * Finds all jokes including search query
+ */
+export const searchByQuery = (searchQuery, original) => {
+  if (!isArray(original)) { return []; }
+  return original.filter(item => {
+    const target = String(item.joke).toLowerCase();
+    const query = String(searchQuery).toLowerCase();
+    return target.includes(query);
+  })
+};
 
-const addCategory = (category, original) => !arrayIntersection(original, [category]).length ?
-  [...original, category] : original;
+/**
+ * Gets jokes by category
+ */
+export const filterByCategories = (categories, original) => {
+  if (!isArray(categories) || !isArray(original)) { return []; }
+  return original.filter(item => arrayIntersection(item.categories, categories).length);
+};
 
-const removeCategory = (category, original) => {
-  if (!arrayIntersection(original, [category]).length) {
-    return original;
-  }
+/**
+ * Add category into selected list
+ */
+export const addCategory = (category, original) => {
+  if (!isArray(original)) { return []; }
+  return !arrayIntersection(original, [category]).length ? [...original, category] : original;
+};
+
+/**
+ * Removes category from selected list
+ */
+export const removeCategory = (category, original) => {
+  if (!isArray(original)) { return []; }
+  if (!arrayIntersection(original, [category]).length) { return original; }
   const index = findIndex(original, category);
   return immutability(original, { $splice: [[index, 1]] });
 }
 
-const nextPage = (count, original) => original + count;
-const prevPage = (count, original) => original - count;
+/**
+ * Scrolls pages
+ */
+export const nextPage = (count, original) => {
+  if (!isInteger(count) || !isInteger(original)) { return 1; }
+  return original + count;
+}
+export const prevPage = (count, original) => {
+  if (!isInteger(count) || !isInteger(original)) { return 1; }
+  return original - count;
+}
 
 immutability.extend('$searchByQuery', searchByQuery);
 immutability.extend('$filterByCategories', filterByCategories);
---
+++ test/index.spec.js
@@ -0,0 +1,58 @@
+import expect from 'expect.js';
+import * as ImmutabilityService from '../src/services/ImmutabilityService';
+
+describe('Immutability service', () => {
+  it('Creates an array of unique, intersecting values from provided arrays', () => {
+    expect(ImmutabilityService.arrayIntersection).to.be.a('function');
+    expect(ImmutabilityService.arrayIntersection([1, 2, 'asd'], [undefined, 2, 'asd', 0, null])).to.eql([2, 'asd']);
+    expect(ImmutabilityService.arrayIntersection(null, [])).to.eql([]);
+  });
+
+  it('Returns index of provided element in array', () => {
+    expect(ImmutabilityService.findIndex).to.be.a('function');
+    expect(ImmutabilityService.findIndex([1, 2, 'asd'], 2)).to.eql(1);
+    expect(ImmutabilityService.findIndex(null, [])).to.eql(false);
+  });
+
+  it('Slices contents from data according to provided pagination', () => {
+    expect(ImmutabilityService.paginationSlice).to.be.a('function');
+    expect(ImmutabilityService.paginationSlice({ page: 2, perPage: 2 }, [1, 2, 'asd', 3, 123, 2])).to.eql(['asd', 3]);
+    expect(ImmutabilityService.paginationSlice(null, [])).to.eql([]);
+  });
+
+  it('Finds all jokes including search query', () => {
+    expect(ImmutabilityService.searchByQuery).to.be.a('function');
+    expect(ImmutabilityService.searchByQuery('ds', [{ joke: 'asd'}, { joke: 'dsa'}, { joke: undefined }])).to.eql([{ joke: 'dsa'}]);
+    expect(ImmutabilityService.searchByQuery(null, [])).to.eql([]);
+  });
+
+  it('Finds all jokes including search query', () => {
+    expect(ImmutabilityService.filterByCategories).to.be.a('function');
+    expect(ImmutabilityService.filterByCategories(['dsa', 'asd'], [{ categories: ['asd']}, { categories: ['dsa']}, { categories: undefined }])).to.eql([{ categories: ['asd']}, { categories: ['dsa']}]);
+    expect(ImmutabilityService.filterByCategories(null, [])).to.eql([]);
+  });
+
+  it('Add category into selected list', () => {
+    expect(ImmutabilityService.addCategory).to.be.a('function');
+    expect(ImmutabilityService.addCategory('dsa', ['bas', 'asd'])).to.eql(['bas', 'asd', 'dsa']);
+    expect(ImmutabilityService.addCategory(null, null)).to.eql([]);
+  });
+
+  it('Removes category from selected list', () => {
+    expect(ImmutabilityService.removeCategory).to.be.a('function');
+    expect(ImmutabilityService.removeCategory('asd', ['bas', 'asd', 'dsa'])).to.eql(['bas', 'dsa']);
+    expect(ImmutabilityService.removeCategory(null, null)).to.eql([]);
+  });
+
+  it('Scrolls page up', () => {
+    expect(ImmutabilityService.nextPage).to.be.a('function');
+    expect(ImmutabilityService.nextPage(1, 4)).to.eql(5);
+    expect(ImmutabilityService.nextPage(null, null)).to.eql(1);
+  });
+
+  it('Scrolls page down', () => {
+    expect(ImmutabilityService.prevPage).to.be.a('function');
+    expect(ImmutabilityService.prevPage(1, 4)).to.eql(3);
+    expect(ImmutabilityService.prevPage(null, null)).to.eql(1);
+  });
+});
